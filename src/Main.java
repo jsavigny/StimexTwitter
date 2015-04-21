1bd728dad6130c20e003dab933d6b6e7a942d322
jsavigny <jsavigny@gmail.com>
2015-04-21 19:16:03 +0700

    Arguments added

--- src/Main.java
+++ src/Main.java
@@ -11,33 +11,33 @@ public class Main {
 
     public static void main(String[] args) throws TwitterException {
         Scanner in = new Scanner(System.in);
-        System.out.println("Masukkan Pilihan Topik :"); //sekarang masukin manual, nanti tergantung dari pilihan
-        System.out.println("1.Olahraga");
-        System.out.println("2.Entertainment");
-        System.out.println("3.Otomotif");
-        String pilihan = in.nextLine();
+        for (String s: args) {
+            System.out.println(s);
+        }
+
+        String pilihan = args[0];
+        String hashKeyWord = args[1];
+        ArrayList<String> keyWords = new ArrayList<>();
+        keyWords.add(args[2]);
+        keyWords.add(args[3]);
+        keyWords.add(args[4]);
+        String whatAlgorithm = args[5];
         int whatTopic = Integer.parseInt(pilihan);
         Topic topic;
         if (whatTopic==1) { //Olahraga
             topic = new Topic("Olahraga", "Sepakbola,Basket,Renang");
-            topic.getCategory().get(0).setKeyWord("sepakbola,persib,gol"); //keyword manual, nanti dari inputan user di web
-            topic.getCategory().get(1).setKeyWord("dunk,miami heat");
-            topic.getCategory().get(2).setKeyWord("kolam,tenggelam");
         } else if (whatTopic==2) { //Entertainment
             topic = new Topic("Entertainment", "Film,Game,Musik");
-            topic.getCategory().get(0).setKeyWord("ff7, star wars, fifty shades of grey");
-            topic.getCategory().get(1).setKeyWord("dota,warcraft,skyrim");
-            topic.getCategory().get(2).setKeyWord("k-pop,rock,afgan");
         } else if (whatTopic==3) { //Otomotif
-            topic = new Topic("Otomotif", "Mobil,Motor,F1");
-            topic.getCategory().get(0).setKeyWord("lamborghini,tune up,mesin");
-            topic.getCategory().get(1).setKeyWord("yamaha,honda");
-            topic.getCategory().get(2).setKeyWord("ferrari,menang,sirkuit");
+            topic = new Topic("Otomotif", "Mobil,Motor,Balapan");
         } else if (whatTopic==4) { //Topik lainnya (Mau Travelling/Technology nih?)
-            topic = new Topic("DUMMY","1,2,3");
+            topic = new Topic("Travelling","Gunung,Pantai,Kota");
         } else {
             topic = new Topic("DUMMY","1,2,3");
         }
+        topic.getCategory().get(0).setKeyWord(keyWords.get(0));
+        topic.getCategory().get(1).setKeyWord(keyWords.get(1));
+        topic.getCategory().get(2).setKeyWord(keyWords.get(2));
         System.out.println("Masukkan Keyword Pencarian Twitter :");
         String twitKeyWord = in.nextLine();
         DataUjiTwitter Streamer = new DataUjiTwitter();
